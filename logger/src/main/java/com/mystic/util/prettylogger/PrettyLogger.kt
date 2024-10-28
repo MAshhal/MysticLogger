b8e938f48f4892d04507720df705e28f226db01a
Muhammad Ashhal <96419703+MAshhal@users.noreply.github.com>
2024-10-28 13:22:11 +0500

    Adds printer and adapter management to PrettyLogger.

--- logger/src/main/java/com/mystic/util/prettylogger/PrettyLogger.kt
+++ logger/src/main/java/com/mystic/util/prettylogger/PrettyLogger.kt
@@ -1,5 +1,9 @@
 package com.mystic.util.prettylogger
 
+import com.mystic.util.prettylogger.adapter.LogAdapter
+import com.mystic.util.prettylogger.printer.LoggerPrinter
+import com.mystic.util.prettylogger.printer.Printer
+
 /**
  * Created using Android Studio Ladybug
  * User: mE
@@ -11,6 +15,98 @@ interface PrettyLogger {
 
     val loggerTag: String
         get() = getTag(javaClass)
+}
+
+object PrinterHolder {
+    var printer: Printer = LoggerPrinter()
+}
+
+fun PrettyLogger.getPrinter(): Printer {
+    return PrinterHolder.printer
+}
+
+fun PrettyLogger.setPrinter(printer: Printer) {
+    PrinterHolder.printer = printer
+}
+
+fun PrettyLogger.addAdapter(adapter: LogAdapter) {
+    PrinterHolder.printer.addAdapter(adapter)
+}
+
+fun PrettyLogger.clearAdapters() {
+    PrinterHolder.printer.clearLogAdapters()
+}
+
+/**
+ * The tag for the coming log.
+ * Note: this tag is used once only. For every log tag, you should config
+ */
+fun PrettyLogger.tag(tag: String?): Printer {
+    return PrinterHolder.printer.tag(tag)
+}
+
+fun PrettyLogger.debug(message: Any?, throwable: Throwable? = null) {
+    PrinterHolder.printer.debug(message, throwable)
+}
+
+fun PrettyLogger.verbose(message: Any?) {
+    PrinterHolder.printer.verbose(message)
+}
+
+fun PrettyLogger.info(message: Any?) {
+    PrinterHolder.printer.info(message)
+}
+
+fun PrettyLogger.warn(message: Any?, throwable: Throwable? = null) {
+    PrinterHolder.printer.warn(message, throwable)
+}
+
+fun PrettyLogger.error(message: Any?, throwable: Throwable? = null) {
+    PrinterHolder.printer.error(message, throwable)
+}
+
+fun PrettyLogger.wtf(message: Any?, throwable: Throwable? = null) {
+    PrinterHolder.printer.wtf(message, throwable)
+}
+
+fun PrettyLogger.debugJson(json: String?) {
+    PrinterHolder.printer.debugJson(json)
+}
+
+fun PrettyLogger.debugXml(xml: String?) {
+    PrinterHolder.printer.debugXml(xml)
+}
+
+fun PrettyLogger.verbose(message: () -> Any?) {
+    PrinterHolder.printer.verbose(message)
+}
+
+fun PrettyLogger.debug(throwable: Throwable? = null, message: () -> Any?) {
+    PrinterHolder.printer.debug(throwable, message)
+}
+
+fun PrettyLogger.info(message: () -> Any?) {
+    PrinterHolder.printer.info(message)
+}
+
+fun PrettyLogger.warn(throwable: Throwable? = null, message: () -> Any?) {
+    PrinterHolder.printer.warn(throwable, message)
+}
+
+fun PrettyLogger.error(throwable: Throwable? = null, message: () -> Any?) {
+    PrinterHolder.printer.error(throwable, message)
+}
+
+fun PrettyLogger.wtf(throwable: Throwable? = null, message: () -> Any?) {
+    PrinterHolder.printer.wtf(throwable, message)
+}
+
+fun PrettyLogger.debugJson(json: () -> String?) {
+    PrinterHolder.printer.debugJson(json)
+}
+
+fun PrettyLogger.debugXml(xml: () -> String?) {
+    PrinterHolder.printer.debugXml(xml)
 }
 
 private fun getTag(jClass: Class<*>): String {
